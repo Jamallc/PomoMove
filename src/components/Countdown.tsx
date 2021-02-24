9d9f3c39d5d297b9c204cef1e35486a0160c1641
Jamallc <weberrocha15@gmail.com>
2021-02-24 13:20:49 -0300

    feat countDown funcion + start cycle button

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,50 @@
-import styles from '../styles/components/CountDown.module.css'
+import { useState, useEffect } from "react";
+import styles from "../styles/components/CountDown.module.css";
 
 export function CountDown() {
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountDown() {
+    setIsActive(true);
+
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    }
+  }, [isActive, time])
+
   return (
-    <div className={styles.countDownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countDownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      <button 
+        type="button" 
+        className={styles.countdownButton}
+        onClick={startCountDown}
+      >
+        Iniciar um cíclo
+      </button>
     </div>
-  )
+  );
 }
